10499ed0ccc12fd3de6995bb4bb711d079dfe820
Ruoyu Qian <rqian@gobonfire.com>
2018-12-26 23:46:14 -0500

    added search model

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -36,8 +37,9 @@ urlpatterns = [
     url(r'^about/$', about_page, name='about'),
     url(r'^login/$', login_page, name='login'),
     url(r'^register/$', register_page, name='register'),
-    url(r'^products/', include("products.urls",
-                               namespace='products'), name='product'),
+    #url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
+    url(r'^products/', include("products.urls",namespace='products')),
+    url(r'^search/', include("search.urls",namespace='search')),
     # url(r'^featured/$', ProductFeaturedListView.as_view()),
     # url(r'^featured/(?P<pk>\d+)$', ProductFeaturedDetailView.as_view()),
     # url(r'^products/$', ProductListView.as_view()),
