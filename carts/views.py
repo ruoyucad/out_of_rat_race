cd0707a24414e2ae00e6250d4fd0ca225fee39d2
Ruoyu Qian <rqian@gobonfire.com>
2018-12-27 23:27:29 -0500

    add / remove functions added to the Cart component

--- carts/views.py
+++ carts/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from products.models import Product
 from .models import Cart
 
 
 def cart_home(request):
-    cart_id = request.session.get('cart_id', None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print('cart id exist')
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
-    return render(request, 'carts/home.html', {})
+
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, 'carts/home.html', {'cart': cart_obj})
+
+
+def cart_update(request):
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id = product_id)
+        except Product.DoesNotExist:
+            print(f'{product_id} is gone?')
+            return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
+        request.session['cart_items'] = cart_obj.products.count()
+    return redirect("cart:home")
